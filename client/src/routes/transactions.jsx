7813640a8fd4140bcf6196bd252d18201cc69fe9
Akash NP <akashnp1998@gmail.com>
2023-03-26 20:58:15 +0530

    feat: add progress bar for route/data loading
    improve feedback to user by avoiding confusing scenarios where user might
    think page is stuck.

--- client/src/routes/transactions.jsx
+++ client/src/routes/transactions.jsx
@@ -1,16 +1,18 @@
 import { useLoaderData } from "react-router-dom";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css"
 
 import TransactionTable from "../components/TransactionTable";
 import { fetchTransactions } from "../lib/dataFetch";
 
 export default function Transactions() {
     const transactions = useLoaderData();
-    return (
-        <TransactionTable transactions={transactions}/>
-    )
+    return <TransactionTable transactions={transactions}/>
 }
 
 export async function loader() {
+    NProgress.start()
     const transactions = await fetchTransactions();
+    NProgress.done()
     return transactions
 }
