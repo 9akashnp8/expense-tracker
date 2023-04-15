ef2c285bf6da4bed741748701ce2bbf3e0578520
Akash NP <akashnp1998@gmail.com>
2023-04-15 20:59:18 +0530

    feat: migrate transaction table component to material ui

--- client/src/components/TransactionTable.jsx
+++ client/src/components/TransactionTable.jsx
@@ -1,74 +1,43 @@
-import { Space, Table } from 'antd';
-const columns = [
-    {
-        title: 'Amount',
-        dataIndex: 'amount',
-        key: 'amount',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Item',
-        dataIndex: 'item',
-        key: 'item',
-    },
-    {
-        title: 'Transaction Date',
-        dataIndex: 'transaction_date_time',
-        key: 'transaction_date_time',
-        render: (_, data) => (
-            <>{new Date(_).toDateString()}</>
-        )
-    },
-    {
-        title: 'Category',
-        dataIndex: 'category',
-        key: 'category',
-    },
-    {
-        title: 'Labels',
-        dataIndex: 'label',
-        key: 'label',
-    },
-    // {
-    //     title: 'Labels',
-    //     key: 'labels',
-    //     dataIndex: 'labels',
-    //     render: (_, { labels }) => (
-    //         <>
-    //             {labels.map((label) => {
-    //                 let color = label.length > 5 ? 'geekblue' : 'green';
-    //                 if (label === 'loser') {
-    //                     color = 'volcano';
-    //                 }
-    //                 return (
-    //                     <Tag color={color} key={label}>
-    //                         {label.toUpperCase()}
-    //                     </Tag>
-    //                 );
-    //             })}
-    //         </>
-    //     ),
-    // },
-    // {
-    //     title: 'Action',
-    //     key: 'action',
-    //     render: (_, record) => (
-    //         <Space size="middle">
-    //             <a>Invite {record.name}</a>
-    //             <a>Delete</a>
-    //         </Space>
-    //     ),
-    // },
-];
-// const TransactionTable = ({transactions}) => <Table columns={columns} dataSource={transactions} />;
-// export default TransactionTable;
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 export default function TransactionTable({transactions}) {
     return (
-        <Table
-            style={{padding: "3em"}}
-            columns={columns}
-            dataSource={transactions}
-        />
-    )
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Item/Payee</TableCell>
+                        <TableCell align="center">Amount</TableCell>
+                        <TableCell align="center">Category</TableCell>
+                        <TableCell align="center">Amount</TableCell>
+                        <TableCell align="center">Date</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {transactions.map((transaction) => (
+                        <TableRow
+                            key={transaction.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {transaction.item}
+                            </TableCell>
+                            <TableCell align="center">{transaction.amount}</TableCell>
+                            <TableCell align="center">{transaction.category}</TableCell>
+                            <TableCell align="center">{transaction.amount}</TableCell>
+                            <TableCell align="center">{transaction.transaction_date_time}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
