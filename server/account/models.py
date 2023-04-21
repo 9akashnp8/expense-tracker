df3be44ad18a3cb81de39954dc78742e755cdebf
Akash NP <akashnp1998@gmail.com>
2023-04-21 22:48:09 +0530

    feat: migrate BE to django/drf from FastAPI
    migrate for better DX as all common use cases are already implemented.
    Eg: Database queryies via the Django ORM, Users and Authentication
    and much more

---
+++ server/account/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class AccountType(models.Model):
+    """
+    model to define type of account i.e., asset class or liability
+    """
+    name = models.CharField(max_length=255)
+    is_asset_class = models.BooleanField()
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Account(models.Model):
+    """
+    main account model from which money is deposited/used
+    """
+    name = models.CharField(max_length=255)
+    type = models.ForeignKey(AccountType, null=True, on_delete=models.SET_NULL)
+    opening_date = models.DateField()
+    starting_balance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    latest_balance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    default_currency = models.CharField(max_length=3)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ server/transaction/models.py
@@ -0,0 +1,51 @@
+# Django & DRF Imports
+from django.db import models
+from django.utils import timezone
+
+# Internal Imports
+from account.models import Account
+
+# Create your models here.
+class CategoryGroup(models.Model):
+    """parent category"""
+    name = models.CharField(max_length=255)
+    is_expense = models.BooleanField()
+    is_budgeted = models.BooleanField()
+    budget_type = models.CharField(max_length=100, null=True, blank=True)
+    budget = models.DecimalField(max_digits=4, decimal_places=2)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Category(models.Model):
+    """main category grouped by CategoryGroup"""
+    name = models.CharField(max_length=255)
+    group = models.ForeignKey(CategoryGroup, null=True, on_delete=models.SET_NULL)
+    is_budgeted = models.BooleanField()
+    budget_type = models.CharField(max_length=100, null=True, blank=True)
+    budget = models.DecimalField(max_digits=4, decimal_places=2)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Label(models.Model):
+    """label class"""
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Transaction(models.Model):
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=4, decimal_places=2)
+    txn_date_time = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    account = models.ForeignKey(Account, null=True, on_delete=models.SET_NULL)
+    label = models.ManyToManyField(Label)
+    notes = models.CharField(max_length=255, null=True, blank=True)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.item
