db2e4ea4018bc33ac6bd0e683a12e163717bb622
Akash NP <akashnp1998@gmail.com>
2023-04-24 17:08:22 +0530

    fix: increase decimal max digits in balance fields

--- server/account/models.py
+++ server/account/models.py
@@ -19,8 +19,8 @@ class Account(models.Model):
     name = models.CharField(max_length=255)
     type = models.ForeignKey(AccountType, null=True, on_delete=models.SET_NULL)
     opening_date = models.DateField()
-    starting_balance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
-    latest_balance = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    starting_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    latest_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     default_currency = models.CharField(max_length=3)
     created_on = models.DateField(auto_now_add=True)
 
