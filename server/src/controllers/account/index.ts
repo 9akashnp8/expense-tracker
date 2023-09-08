0f31c60e99fad408e542a03d098aa9110e44365a
Akash NP <akashnp1998@gmail.com>
2023-09-08 12:19:24 +0400

    chore: rename to listAccounts for better understanding

--- server/src/controllers/account/index.ts
+++ server/src/controllers/account/index.ts
@@ -2,7 +2,7 @@ import { Request, Response} from 'express';
 import { supabase } from '../../lib/supabase.js';
 
 
-export async function getAccounts(req: Request, res: Response) {
+export async function listAccounts(req: Request, res: Response) {
     const { data, error } = await supabase
         .from('account')
         .select()
