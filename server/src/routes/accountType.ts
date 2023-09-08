5b74052b281af1b2583018f0f79e64705d80e3ba
Akash NP <akashnp1998@gmail.com>
2023-09-08 13:21:23 +0400

    feat: complete CRUD for accountType feature

--- server/src/routes/accountType.ts
+++ server/src/routes/accountType.ts
@@ -1,7 +1,17 @@
-import express, { Request, Response} from 'express';
+import express from 'express';
+
+import {
+    listAccountType,
+    getAccountType,
+    createAccountType,
+    updateAccountType,
+    deleteAccountType
+} from '../controllers/accountType/index.js';
 
 export const router = express.Router()
 
-router.get("/", (req: Request, res: Response) => {
-    res.send("Account Type Root")
-})
+router.get("/", listAccountType)
+router.get("/:id", getAccountType)
+router.post("/", createAccountType)
+router.patch("/:id", updateAccountType)
+router.delete("/:id", deleteAccountType)
