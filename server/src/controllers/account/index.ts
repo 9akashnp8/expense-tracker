d79f7371dc22396b9cac319000e48512a2e2075d
Akash NP <akashnp1998@gmail.com>
2023-09-08 12:21:11 +0400

    feat: complete CRUD for account feature

--- server/src/controllers/account/index.ts
+++ server/src/controllers/account/index.ts
@@ -1,5 +1,6 @@
 import { Request, Response} from 'express';
 import { supabase } from '../../lib/supabase.js';
+import { createSuccessResponse, createFailureResponse } from '../../utils/response.js';
 
 
 export async function listAccounts(req: Request, res: Response) {
@@ -7,5 +8,51 @@ export async function listAccounts(req: Request, res: Response) {
         .from('account')
         .select()
 
-    res.json(data)
+    if (error) return createFailureResponse(req, res, 500)
+    return createSuccessResponse(req, res, 200, "", data)
+}
+
+export async function getAccount(req: Request<{ id: string}>, res: Response) {
+    const { id } = req.params
+    const { data, error } = await supabase
+        .from('account')
+        .select()
+        .eq('id', id)
+
+    if (error) return createFailureResponse(req, res, 500)
+    else if (!data?.length) return createFailureResponse(req, res, 404, "not-found")
+    return createSuccessResponse(req, res, 200, "", data)
+}
+
+export async function createAccount(req: Request<{ id: string}>, res: Response) {
+    const body = req.body
+    const { error } = await supabase
+        .from('account')
+        .insert(body)
+
+    if (error) return createFailureResponse(req, res, 500, "", error)
+    return createSuccessResponse(req, res, 201, "", body)
+}
+
+export async function updateAccount(req: Request, res: Response) {
+    const { id } = req.params
+    const body = req.body
+    const { error } = await supabase
+        .from('account')
+        .update(body)
+        .eq('id', id)
+
+    if (error) return createFailureResponse(req, res, 500, "", error)
+    return createSuccessResponse(req, res, 204)
+}
+
+export async function deleteAccount(req: Request, res: Response) {
+    const { id } = req.params
+    const { error } = await supabase
+        .from('account')
+        .delete()
+        .eq('id', id)
+
+    if (error) return createFailureResponse(req, res, 500, "", error)
+    return createSuccessResponse(req, res, 204)
 }
--- server/src/routes/account.ts
+++ server/src/routes/account.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
-import { getAccounts } from '../controllers/account/index.js';
+import { getAccount, listAccounts, createAccount, updateAccount, deleteAccount } from '../controllers/account/index.js';
 
 export const router = express.Router()
 
-router.get('/', getAccounts)
+router.get('/', listAccounts)
+router.post('/', createAccount)
+router.get('/:id', getAccount)
+router.patch('/:id', updateAccount)
+router.delete('/:id', deleteAccount)
