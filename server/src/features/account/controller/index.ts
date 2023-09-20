f8cf77bd1a0cc9f5fa76206df7969274347b0da3
Akash NP <akashnp1998@gmail.com>
2023-09-20 15:16:23 +0400

    style: setup prettier, configure with husky to run before commit

--- server/src/features/account/controller/index.ts
+++ server/src/features/account/controller/index.ts
@@ -1,19 +1,19 @@
-import { Request, Response} from "express";
+import { Request, Response } from "express";
 import {
     createSuccessResponse,
-    createFailureResponse
+    createFailureResponse,
 } from "../../common/utils/response.js";
 import {
     listAccounts,
     getAccount,
     createAccount,
     updateAccount,
-    deleteAccount
+    deleteAccount,
 } from "../service/index.js";
 import { Account } from "../types.js";
 import { logger as rootLogger } from "../../common/utils/logger.js";
 
-const logger = rootLogger.child({ feature: "account" })
+const logger = rootLogger.child({ feature: "account" });
 
 export async function listAccountsController(req: Request, res: Response) {
     const { data, error } = await listAccounts();
@@ -22,71 +22,70 @@ export async function listAccountsController(req: Request, res: Response) {
         logger.error({
             message: "error when querying accounts",
             error: error,
-        })
-        return createFailureResponse(req, res, 500)
+        });
+        return createFailureResponse(req, res, 500);
     }
-    return createSuccessResponse(req, res, 200, "", data)
+    return createSuccessResponse(req, res, 200, "", data);
 }
 
 export async function getAccountController(req: Request, res: Response) {
-    const { id } = req.params // TODO: ensure id is number
-    const { data, error } = await getAccount(id)
+    const { id } = req.params; // TODO: ensure id is number
+    const { data, error } = await getAccount(id);
 
     if (error) {
         logger.error({
             message: `error when querying account with id: ${id}`,
             error: error,
-        })
-        return createFailureResponse(req, res, 500)
+        });
+        return createFailureResponse(req, res, 500);
+    } else if (!data?.length) {
+        logger.error({ message: `account with id: ${id} not found` });
+        return createFailureResponse(req, res, 404, "not-found");
     }
-    else if (!data?.length) {
-        logger.error({ message: `account with id: ${id} not found` })
-        return createFailureResponse(req, res, 404, "not-found")
-    }
-    return createSuccessResponse(req, res, 200, "", data)
+    return createSuccessResponse(req, res, 200, "", data);
 }
 
 export async function createAccountController(req: Request, res: Response) {
-    const body: Account = req.body
-    const { error } = await createAccount(body)
+    const body: Account = req.body;
+    const { error } = await createAccount(body);
 
     if (error) {
         logger.error({
             message: "error when creating account",
             requestPayload: body,
             error: error,
-        })
-        return createFailureResponse(req, res, 500, "")
+        });
+        return createFailureResponse(req, res, 500, "");
     }
-    return createSuccessResponse(req, res, 201, "", body)
+    return createSuccessResponse(req, res, 201, "", body);
 }
 
 export async function updateAccountController(req: Request, res: Response) {
-    const { id } = req.params
-    const body = req.body
-    const { error } = await updateAccount(id, body)
+    const { id } = req.params;
+    const body = req.body;
+    const { error } = await updateAccount(id, body);
 
     if (error) {
         logger.error({
             message: `error when updating account: ${id}`,
             requestPayload: body,
             error: error,
-        })
-        return createFailureResponse(req, res, 500, "")
+        });
+        return createFailureResponse(req, res, 500, "");
     }
-    return createSuccessResponse(req, res, 204)
+    return createSuccessResponse(req, res, 204);
 }
 
 export async function deleteAccountController(req: Request, res: Response) {
-    const { id } = req.params
-    const { error } = await deleteAccount(id)
+    const { id } = req.params;
+    const { error } = await deleteAccount(id);
 
     if (error) {
         logger.error({
             message: `error when deleting account: ${id}`,
             error: error,
-        })
-        return createFailureResponse(req, res, 500, "")
+        });
+        return createFailureResponse(req, res, 500, "");
     }
-    return createSuccessResponse(req, res, 204)
+    return createSuccessResponse(req, res, 204);
 }
