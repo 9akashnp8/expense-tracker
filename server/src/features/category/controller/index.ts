dd3da65aa3f085dce4452fae1e13b276ccb3c0d4
Akash NP <akashnp1998@gmail.com>
2023-09-25 21:58:55 +0400

    chore: update category controllers to service based structure

--- server/src/features/category/controller/index.ts
+++ server/src/features/category/controller/index.ts
@@ -1,23 +1,31 @@
 import { Request, Response } from "express";
-import { supabase } from "../../common/supabase.js";
 import {
     createFailureResponse,
     createSuccessResponse,
 } from "../../common/utils/response.js";
+import {
+    createCategory,
+    deleteCategory,
+    getCategory,
+    listCategory,
+    updateCategory,
+} from "../service/index.js";
+import { CreateCategoryPayload, UpdateCategoryPayload } from "../types.js";
+import { ParamsDictionary } from "express-serve-static-core";
 
-export async function listCategory(req: Request, res: Response) {
-    const { data, error } = await supabase.from("category").select();
+export async function listCategoryController(req: Request, res: Response) {
+    const { data, error } = await listCategory();
 
     if (error) return createFailureResponse(req, res, 500);
     return createSuccessResponse(req, res, 200, "", data);
 }
 
-export async function getCategory(req: Request<{ id: string }>, res: Response) {
+export async function getCategoryController(
+    req: Request<{ id: string }>,
+    res: Response,
+) {
     const { id } = req.params;
-    const { data, error } = await supabase
-        .from("category")
-        .select()
-        .eq("id", id);
+    const { data, error } = await getCategory(id);
 
     if (error) return createFailureResponse(req, res, 500);
     else if (!data?.length)
@@ -25,29 +33,32 @@ export async function getCategory(req: Request<{ id: string }>, res: Response) {
     return createSuccessResponse(req, res, 200, "", data);
 }
 
-export async function createCategory(
-    req: Request<{ id: string }>,
+export async function createCategoryController(
+    req: Request<ParamsDictionary, Response, CreateCategoryPayload>,
     res: Response,
 ) {
     const { body } = req;
-    const { error } = await supabase.from("category").insert(body);
+    const { error } = await createCategory(body);
 
     if (error) return createFailureResponse(req, res, 500, "", error);
     return createSuccessResponse(req, res, 201, "", body);
 }
 
-export async function updateCategory(req: Request, res: Response) {
+export async function updateCategoryController(
+    req: Request<{ id: string }, Response, UpdateCategoryPayload>,
+    res: Response,
+) {
     const { id } = req.params;
     const { body } = req;
-    const { error } = await supabase.from("category").update(body).eq("id", id);
+    const { error } = await updateCategory(id, body);
 
     if (error) return createFailureResponse(req, res, 500, "", error);
     return createSuccessResponse(req, res, 204);
 }
 
-export async function deleteCategory(req: Request, res: Response) {
+export async function deleteCategoryController(req: Request, res: Response) {
     const { id } = req.params;
-    const { error } = await supabase.from("category").delete().eq("id", id);
+    const { error } = await deleteCategory(id);
 
     if (error) return createFailureResponse(req, res, 500, "", error);
     return createSuccessResponse(req, res, 204);
---
+++ server/src/features/category/types.ts
@@ -0,0 +1,22 @@
+export type Category = {
+    id: number;
+    created_at: string;
+    name: string;
+    group: number;
+    is_budgeted: boolean;
+    budget: string;
+};
+
+export type CreateCategoryPayload = {
+    name: string;
+    group: number;
+    is_budgeted: boolean;
+    budget?: string;
+};
+
+export type UpdateCategoryPayload = {
+    name?: string;
+    group?: number;
+    is_budgeted?: boolean;
+    budget?: string;
+};
