795d4e47c8f9d6d55fda22e23d91c757271ac6b0
Akash NP <akashnp1998@gmail.com>
2023-09-28 15:30:26 +0400

    feat: improve account list ui, convert table to cards

---
+++ client/src/features/account/components/AccountDetailCard/index.tsx
@@ -0,0 +1,34 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
+import { Account } from "../../types";
+
+type Props = {
+    account: Account
+}
+
+export default function ({ account }: Props) {
+  const balanceColor = Math.sign(account.latest_balance) === 1 ? "green" : "red";
+
+  return (
+    <Card sx={{ marginTop: 3 }}>
+      <CardContent
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography component="div">{account.name}</Typography>
+        <Typography
+          sx={{
+            color: balanceColor,
+          }}
+        >
+          {account.default_currency + " " + account.latest_balance}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
