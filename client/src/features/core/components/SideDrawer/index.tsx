8b1f044342a3f6135d2b7e044cccbab50522be5a
Akash NP <akashnp1998@gmail.com>
2023-10-02 20:09:56 +0400

    style: fix route children not rendering inside SideDrawer OutLet

--- client/src/features/core/components/SideDrawer/index.tsx
+++ client/src/features/core/components/SideDrawer/index.tsx
@@ -1,32 +1,30 @@
-import * as React from 'react';
-import { Box } from '@mui/material';
-import Drawer from '@mui/material/Drawer';
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import IconButton from '@mui/material/IconButton';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const drawerWidth = 200;
+const drawerWidth = 240;
 
 export default function ({ children, ...props }: any) {
     const { window } = props;
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const container = window !== undefined ? () => window().document.body : undefined;
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
@@ -41,11 +39,49 @@ export default function ({ children, ...props }: any) {
     };
     
     function handleSignOut() {
-        window.location.href = "login"
+    window.location.href = "login";
     }
 
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        <Link
+          to={"account"}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem key={1} disablePadding>
+            <ListItemButton onClick={handleDrawerToggle}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Account"} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+        <Link
+          to={"category"}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <ListItem key={1} disablePadding>
+            <ListItemButton onClick={handleDrawerToggle}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Category"} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
     return (
-        <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
             <CssBaseline />
             <AppBar
                 position="fixed"
@@ -68,7 +104,7 @@ export default function ({ children, ...props }: any) {
                         aria-label="open drawer"
                         edge="start"
                         onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
                     >
                         <MenuIcon />
                     </IconButton>
@@ -85,12 +121,15 @@ export default function ({ children, ...props }: any) {
                         </IconButton>
                         <Menu
                             id="menu-appbar"
+              sx={{
+                marginTop: "-10px",
+                marginLeft: "20px"
+              }}
                             anchorEl={anchorEl}
                             anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
+                vertical: 'bottom',
+                horizontal: 'left',
                             }}
-                            keepMounted
                             transformOrigin={{
                                 vertical: 'top',
                                 horizontal: 'right',
@@ -105,6 +144,11 @@ export default function ({ children, ...props }: any) {
 
                 <Divider />
             </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
             <Drawer
                 container={container}
                 variant="temporary"
@@ -114,71 +158,37 @@ export default function ({ children, ...props }: any) {
                   keepMounted: true, // Better open performance on mobile.
                 }}
                 sx={{
-                  display: { xs: 'block', sm: 'none' },
-                  '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-                }}
-            >
-                <Toolbar />
-                <Box sx={{ overflow: 'auto' }} className={'test2'}>
-                    <List>
-                        <Link to={"account"} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <ListItem key={1} disablePadding>
-                                <ListItemButton onClick={handleDrawerToggle}>
-                                    <ListItemIcon>
-                                        <InboxIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={"Account"} />
-                                </ListItemButton>
-                            </ListItem>
-                        </Link>
-                        <Link to={"category"} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <ListItem key={1} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <InboxIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={"Category"} />
-                                </ListItemButton>
-                            </ListItem>
-                        </Link>
-                    </List>
-                </Box>
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
             </Drawer>
             <Drawer
-                open
                 variant="permanent"
                 sx={{
-                    display: { xs: 'none', sm: 'block' },
-                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
                 }}
-            >
-                <Toolbar />
-                <Box sx={{ overflow: 'auto' }} className={'test2'}>
-                    <List>
-                        <Link to={"account"} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <ListItem key={1} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <InboxIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={"Account"} />
-                                </ListItemButton>
-                            </ListItem>
-                        </Link>
-                        <Link to={"category"} style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <ListItem key={1} disablePadding>
-                                <ListItemButton>
-                                    <ListItemIcon>
-                                        <InboxIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary={"Category"} />
-                                </ListItemButton>
-                            </ListItem>
-                        </Link>
-                    </List>
-                </Box>
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
                 <Toolbar />
                 {children}
             </Box>
