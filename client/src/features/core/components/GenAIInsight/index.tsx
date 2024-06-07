54fafe29a85b13239641cece18a6730e16f2991a
Akash NP <akashnp1998@gmail.com>
2024-06-07 22:30:07 +0400

    feat: add GenAIInsight component

---
+++ client/src/features/core/components/GenAIInsight/index.tsx
@@ -0,0 +1,29 @@
+import { Callout, Flex, Text, Box } from "@radix-ui/themes";
+import { Link } from "react-router-dom";
+
+type Props = {
+  insight: string;
+};
+
+export default function GenAIInsight({ insight }: Props) {
+  return (
+    <Box mb={"5"}>
+      <Callout.Root>
+        <Flex direction={"column"}>
+          <Flex gap={"2"} mb={"2"}>
+            <Callout.Icon>✨</Callout.Icon>
+            <Text weight="medium">
+              <Link
+                to={"/ai-insights"}
+                style={{ textDecoration: "underline", color: "inherit" }}
+              >
+                AI Insights
+              </Link>
+            </Text>
+          </Flex>
+          <Text as="p">{insight}</Text>
+        </Flex>
+      </Callout.Root>
+    </Box>
+  );
+}
--- client/src/features/core/routes/home.tsx
+++ client/src/features/core/routes/home.tsx
@@ -2,21 +2,25 @@ import SummaryWidget from "../components/SummaryWidget";
 import LastWeekSpendChart from "../components/LastWeekSpendChart";
 import CategorySpendChart from "../components/CategorySpendChart";
 import WidgetWrapper from "../components/WidgetWrapper";
+import GenAIInsight from "../components/GenAIInsight";
 
 import { Grid } from "@radix-ui/themes";
 
 export default function HomePage() {
   return (
-    <Grid columns={"2"} rows={"2"} gap={"5"}>
-      <WidgetWrapper>
-        <SummaryWidget />
-      </WidgetWrapper>
-      <WidgetWrapper>
-        <LastWeekSpendChart />
-      </WidgetWrapper>
-      <WidgetWrapper>
-        <CategorySpendChart />
-      </WidgetWrapper>
-    </Grid>
+    <>
+      <GenAIInsight insight="Your Transactions have increased by 10% with changes in most spend in X category" />
+      <Grid columns={{ xs: "1", md: "2" }} rows={"1fr 1fr"} gap={"5"}>
+        <WidgetWrapper>
+          <SummaryWidget />
+        </WidgetWrapper>
+        <WidgetWrapper>
+          <LastWeekSpendChart />
+        </WidgetWrapper>
+        <WidgetWrapper>
+          <CategorySpendChart />
+        </WidgetWrapper>
+      </Grid>
+    </>
   );
 }
